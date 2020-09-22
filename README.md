739c6c7559cdbddd8d73d14458a046ef53cd6889
Alexander Gauss <gauss@ros-cloud.at>
2020-09-22 11:30:14 +0000

    added documentation

--- README.md
+++ README.md
@@ -1,3 +1,106 @@
 # ROS-sdk-js
 
 Das ist die offizielle ROS-SDK (js/ts) und ist dafür gedacht, Applikationen auf Basis der ROS-Cloud Plattform zu entwickeln.
+
+# Benutzung
+
+Um die API zu initialisieren muss sie importiert und initialisiert werden.
+Der Endpoint (z.B. https://api.dev.ros-cloud.at) muss im Constructor als Argument angegeben werden.
+
+```js
+import { rosapi } from "ros-sdk-js"
+
+const api = new rosapi("<ENDPOINT>")
+```
+
+# Token Speichern
+Wenn der Token mithilfe von Firebase erstellt wurde, muss er in der sdk gespeichert werden, um die API auch nutzen zu können.
+
+```js
+try {
+    api.token().set("token")
+}
+catch(err) {
+    // wenn der token invalid ist
+}
+```
+
+# Token löschen
+Wenn der Benutzer sich ausloggt, muss der Token auch gelöscht werden.
+
+```js
+api.token().delete()
+```
+
+# Authentifizierung
+Wenn ein Benutzer sich neu registriert, muss zuerst ein neuer S3 Bucket erstellt werden. 
+
+Benutzer neu Registrieren:
+```js
+api.user().authenticate("register")
+.then(response => {
+    // example response if the user is new
+    {
+        status: true,
+        bucket: true,
+        database: true,
+        user_data: {
+            iss: 'https://securetoken.google.com/ros-cloud-cc711',
+            aud: 'ros-cloud-cc711',
+            auth_time: 1600772629,
+            user_id: 'ZxMByPJPPbSt0mdP4ukFrhNEtDs2',
+            sub: 'ZxMByPJPPbSt0mdP4ukFrhNEtDs2',
+            iat: 1600772629,
+            exp: 1600776229,
+            email: 'johndoewe23@johnmail.net',
+            email_verified: false,
+            firebase: { 
+                identities: [Object], 
+                sign_in_provider: 'password' 
+            },
+            uid: 'ZxMByPJPPbSt0mdP4ukFrhNEtDs2'
+        }
+    }
+
+    // example response if the user already exists
+    { status: false, user: 'existing' }
+})
+```
+
+Benutzer einloggen:
+```js
+api.user().authenticate("login")
+.then(response => {
+    // example response
+    {
+        status: true,
+        login: true,
+        user_data: {
+            iss: 'https://securetoken.google.com/ros-cloud-cc711',
+            aud: 'ros-cloud-cc711',
+            auth_time: 1600773270,
+            user_id: 'biCRN3fA6mYF0TRemT1QQoW5T833',
+            sub: 'biCRN3fA6mYF0TRemT1QQoW5T833',
+            iat: 1600773270,
+            exp: 1600776870,
+            email: 'gregrgqergegr@gmail.com',
+            email_verified: false,
+            firebase: { 
+                identities: [Object], 
+                sign_in_provider: 'password' 
+                },
+            uid: 'biCRN3fA6mYF0TRemT1QQoW5T833'
+        },
+        storage_data: []
+    }
+
+    // Example response if the user isn't registered yet
+    { status: false, user: 'not existing' }
+})
+```
+
+
+
+
+
+
--- tests/index.js
+++ tests/index.js
@@ -1,19 +1,18 @@
 const { rosapi } = require("../lib")
-const { tokenStore } = require("../lib/store")
 
 const api = new rosapi("https://api.dev.ros-cloud.at")
 
-api.token().set("eyJhbGciOiJSUzI1NiIsImtpZCI6IjczNzVhZmY3MGRmZTNjMzNlOTBjYTM2OWUzYTBlZjQxMzE3MmZkODIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcm9zLWNsb3VkLWNjNzExIiwiYXVkIjoicm9zLWNsb3VkLWNjNzExIiwiYXV0aF90aW1lIjoxNjAwNzAzNTM5LCJ1c2VyX2lkIjoiZUxnZU5Ydm1VaFNaODFza1NtdFdlU0hiR3BFMyIsInN1YiI6ImVMZ2VOWHZtVWhTWjgxc2tTbXRXZVNIYkdwRTMiLCJpYXQiOjE2MDA3MDM1MzksImV4cCI6MTYwMDcwNzEzOSwiZW1haWwiOiIxMjEyMTIxMjEyZWpvaXdlcmZAdGVzdG1haWwubmV0IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbIjEyMTIxMjEyMTJlam9pd2VyZkB0ZXN0bWFpbC5uZXQiXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.KjSFDrEqIsyysCVZxR6xXVChsS-H26YywSZAUru4akbNCwdCfVnp4j87sRpVW8UgsHsjF3daGGpG-NHVilCEQOCxRVDdx0XHdRAEGMqqoDvtTTMT_JdQ8wT-laLBBrMQvVci50aF7994y5so80NUgkOfUo3c1a23cjR9_6smQVkw_iKT2t-4aJlVxrFRo8lLAyIHXD4tAJ33FzMm8jpo_C7L2yTS57FyHBUmCRqGpsELVpmWKgHKOeoA0uMaU1kHewbt1wf7dkaItR_rVynyASx5KLINoDAH4_CSPvQTnX1hUEb0EsyC8kxWwTAKv7cVGyOQeJUQjye7eqeWdAYYrw")
+api.token().set("eyJhbGciOiJSUzI1NiIsImtpZCI6IjFlNjYzOGY4NDlkODVhNWVkMGQ1M2NkNDI1MzE0Y2Q1MGYwYjY1YWUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcm9zLWNsb3VkLWNjNzExIiwiYXVkIjoicm9zLWNsb3VkLWNjNzExIiwiYXV0aF90aW1lIjoxNjAwNzczNDQ3LCJ1c2VyX2lkIjoiYkw5dHpMMlBGSlB1OFBXTnFQSFlBNWFnSDRmMiIsInN1YiI6ImJMOXR6TDJQRkpQdThQV05xUEhZQTVhZ0g0ZjIiLCJpYXQiOjE2MDA3NzM0NDcsImV4cCI6MTYwMDc3NzA0NywiZW1haWwiOiJ0dTM1Njd1NTZ1QHR1bWFpbC5uZXQiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsidHUzNTY3dTU2dUB0dW1haWwubmV0Il19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.ODCvPmPo3HVg1pIS_d4Zvsp5ozQKxloZweSVwYNRo2PYhEAHcUnfXh47OASwu6cXhVubrJynvOUOlmwagLYdLx6WnmrHgpsp-SpWshHCfY_wUoETX9aDTYV5SZmHKwztnvGE1AFb6YhubyPc5HNK729YL2W9a7hrxILNyPwRySIPAvcBvcXE6kfk2tbKWb-ez4ulLIpxdDLnIfigw-x1UBWRsgV4NDStEQpAQSD9mY9Uhp6U8jUcwBz_AwtemG7fLtoJwQ4pLobUaDcS7xZJjLw6iUla9yQ5up58IlMf8Mgix3FURNzDiRnSW8uZ8oDfmoLkn6Q8NB455yBAvgQnLw")
 
-// api.user().authenticate("login")
-// .then(response => {
-//     console.log(response)
-//     console.log("everything went awesome")
-// })
-// .catch(err => {
-//     console.log("an error occured")
-//     console.log(err)
-// })
+api.user().authenticate("login")
+.then(response => {
+    console.log(response)
+    console.log("everything went awesome")
+})
+.catch(err => {
+    console.log("an error occured")
+    console.log(err)
+})
 
 // api.user().deleteNow()
 // .then(response => {
@@ -25,11 +24,11 @@ api.token().set("eyJhbGciOiJSUzI1NiIsImtpZCI6IjczNzVhZmY3MGRmZTNjMzNlOTBjYTM2OWU
 //     console.log(err)
 // })
 
-api.object().createDir("/Doc2", "Doc1")
-.then(response => {
-    console.log(response)
-})
-.catch(err => {
-    console.log(err)
-})
+// api.object().createDir("/Doc2", "Doc1")
+// .then(response => {
+//     console.log(response)
+// })
+// .catch(err => {
+//     console.log(err)
+// })
 
